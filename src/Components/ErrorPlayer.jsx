e764cda2c5b3a276cb5f93d9debf07f2de200763
Abdelilah <abdelilah.zarzor@gmail.com>
2021-07-07 14:09:16 +0100

    -added dark mode to error page

--- src/Components/ErrorPlayer.jsx
+++ src/Components/ErrorPlayer.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const ErrorPlayer = () => {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <div className="flex flex-col justify-center items-center h-screen w-screen dark:bg-black dark:text-white">
         <h1 className="text-2xl">
           cannot access game before setting the player names
         </h1>
         <button
           type="submit"
-          className="mt-10 text-xl p-1 pl-6 pr-6 rounded-md font-bold bg-black text-white transform hover:scale-105"
+          className="mt-10 text-xl p-1 pl-6 pr-6 rounded-md font-bold bg-black text-white transform hover:scale-105 dark:text-black dark:bg-white"
         >
           <a href="/"> Go home</a>
         </button>
